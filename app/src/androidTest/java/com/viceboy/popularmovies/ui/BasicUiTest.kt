a791ba6794bc93262b3acaef7043038319cab9bd
sujhaglobe <sumit.jha@newglobe.education>
2021-04-30 22:24:26 +0530

    Add test activity

--- app/src/androidTest/java/com/viceboy/popularmovies/ui/BasicUiTest.kt
+++ app/src/androidTest/java/com/viceboy/popularmovies/ui/BasicUiTest.kt
@@ -6,11 +6,13 @@ import androidx.annotation.IdRes
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
-import com.viceboy.popularmovies.MainActivity
 import com.viceboy.popularmovies.R
+import com.viceboy.popularmovies.TestGenActivity
 import com.viceboy.popularmovies.ui.RecyclerViewAssertions.itemViewMatches
 import junit.framework.Assert.*
 import org.hamcrest.Matcher
@@ -23,17 +25,40 @@ class BasicUiTest {
 
     @Rule
     @JvmField
-    var mActivityTestRule : ActivityTestRule<MainActivity> =
-        ActivityTestRule(MainActivity::class.java, true, true)
+    var mActivityTestRule : ActivityTestRule<TestGenActivity> =
+        ActivityTestRule(TestGenActivity::class.java, true, true)
 
 
     @Test
     fun test1() {
-        SystemClock.sleep(25000)
+        SystemClock.sleep(10000)
         onView(
-            withId(R.id.rvMovies)).
-        check(itemViewMatches(0, R.id.tvTitle,
-            withEffectiveVisibility(Visibility.GONE)))
+            withId(R.id.txt)).
+        check(matches(withText("Test")))
+        onView(withId(R.id.btnTest)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+    }
+
+    @Test
+    fun test2() {
+        SystemClock.sleep(10000)
+        onView(
+            withId(R.id.txt)).
+        check(matches(withText("Test")))
+        onView(
+            withId(R.id.btnTest)).
+        check(matches(withText("Button1")))
+    }
+
+    @Test
+    fun test3() {
+        SystemClock.sleep(10000)
+        onView(
+            withId(R.id.txt)).
+        check(matches(withText("Test")))
+        onView(
+            withId(R.id.btnTest)).
+        check(matches(withText("Button")))
+        onView(withId(R.id.btnTest)).check(matches(withEffectiveVisibility(Visibility.GONE)))
     }
 }
 
---
+++ app/src/main/java/com/viceboy/popularmovies/TestGenActivity.kt
@@ -0,0 +1,11 @@
+package com.viceboy.popularmovies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestGenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_gen)
+    }
+}
