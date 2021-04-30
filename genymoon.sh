27a9e813f1a43f5442fad4dc45e13772e908bd78
sujhaglobe <sumit.jha@newglobe.education>
2021-04-30 23:12:27 +0530

    Updated scrupt

--- genymoon.sh
+++ genymoon.sh
@@ -5,4 +5,5 @@ serialId=`gmsaas instances adbconnect "${deviceId}"`
 gmsaas instances list
 echo "Serial Id :"${serialId}
 echo "Device Id: " ${deviceId}
+java -jar /home/mango/Downloads/spoon-runner.jar --sdk /home/mango/Android/Sdk --apk /home/mango/StudioProjects/PopularMovies/app/build/outputs/apk/debug/app-debug.apk --test-apk /home/mango/StudioProjects/PopularMovies/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk -serial ${serialId}  --debug --fail-on-failure --no-animations --grant-all
 gmsaas instances stop "${deviceId}"
